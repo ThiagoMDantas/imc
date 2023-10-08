f631cde2fd50d2bb9630f6e193cf054705aa13e7
Thiago Martins Dantas Silva <thiagomarins.dantas@gmail.com>
2023-10-08 03:56:15 -0300

    Add modules

--- js/script.js
+++ js/script.js
@@ -1,37 +1,33 @@
+import { Modal } from './modal.js'
+import { AlertError } from './alert-error.js'
+
 const form = document.querySelector('form')
 const inputWeight   = document.querySelector('#weight')
 const inputHeight   = document.querySelector('#height')
-
-const Modal = {
-  wrapper: document.querySelector(".modal-wrapper"),
-  message: document.querySelector(".modal .title span"),
-  buttonClose: document.querySelector(".modal button.close"),
-  
-  open(){
-    Modal.wrapper.classList.add("open")
-  },
-  close(){
-    Modal.wrapper.classList.remove("open")
-  }
-}
 
 form.onsubmit = function(event) {
   event.preventDefault()
 
   const weight = inputWeight.value
   const height = inputHeight.value
+
+  if (notNumber(weight) || notNumber(height)){
+    AlertError.open()
+    return; 
+  } else {
+    AlertError.close()
+  }
+
   const result = IMC(weight, height);
 
   Modal.message.innerText = `Seu IMC é de ${result}`
   Modal.open()
 }
 
-Modal.buttonClose.onclick = () => {
-  inputWeight.value = ""
-  inputHeight.value = ""
-  Modal.close()
-}
-
 function IMC(weight, height){
   return (weight / (height / 100) ** 2 ).toFixed(2);
+}
+
+function notNumber(value){
+  return isNaN(value) || value == ""
 }
